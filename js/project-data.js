df48db8751091f3e5272f0dcd74a51f0a21c72f1
Andreas Gramstad <your.email@example.com>
2026-03-12 18:52:38 +0100

    remove placeholder project card

--- js/project-data.js
+++ js/project-data.js
@@ -25,17 +25,5 @@ export const projects = [
         shortDescription: "Visuell identitet med logo, grafisk profil og bilder som formidler stemningen rundt en kaffebedrift.",
         description: "Vi har designet en komplett grafisk profil for en kaffebedrift, inkludert logo, farger, typografi og visuelle elementer. I tillegg har vi utviklet et bildemateriell som forsterker merkevaren og bidrar til et tydelig, helhetlig uttrykk.",
         link: "https://example.com/project-two"
-    },
-    {
-        id: 3,
-        title: "Zing Bryggeri",
-        image: "images/portfolio/rainydays-card-1.png",
-        images: [
-            "images/portfolio/rainy-fullsize.jpg"
-        ],
-        shortDescription: "Short description for the home page.",
-        description: "This is the full description of Project One.",
-        link: "https://example.com/project-one"
-
     }
 ];
